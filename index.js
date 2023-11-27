20989a6b41fe85c7ede166ed92e0b2a5aa7ef006
Subhan Yousaf <subhanyousaf2003@gmail.com>
2023-11-27 14:17:07 -0800

    :fire: Remove ENV config for now

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const morgan = require('morgan');
-const config = require('./config');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
-const genres = require('./routes/genres');
-const games = require('./routes/games');
+const genres = require("./routes/genres");
+const games = require("./routes/games");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
+
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use("/api/genres", genres);
 app.use("/api/games", games);
 require("./startup/prod")(app);
 
-mongoose.connect(config.DB_URL)
-    .then(() => console.log("Connected to database..."))
-    .catch(err => console.error("Could not connect to database...", err));
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Connected to database..."))
+  .catch((err) => console.error("Could not connect to database...", err));
 
-app.listen(config.PORT, () => console.log(`Listening on port ${config.PORT}...`));
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}...`)
+);
